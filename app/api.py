0054f4110b1976d3e313b591ac6713802cdeb3d8
Wilson Lima <wilson.slima@gmail.com>
2026-04-30 15:45:11 -0300

    Ajustes na API

--- app/api.py
+++ app/api.py
@@ -4,8 +4,12 @@ import os
 
 import pickle
 
+import pandas as pd
 from fastapi import FastAPI
-from fastapi.responses import PlainTextResponse
+from fastapi.responses import (
+    JSONResponse,
+    PlainTextResponse
+)
 from pydantic import BaseModel
 from typing import List
 from sklearn.metrics import classification_report
@@ -14,7 +18,7 @@ from app.src.constants import (
     MODEL_DIR,
     MODEL_FILE,
     X_TEST_FILE,
-    Y_TEST_FILE
+    Y_TEST_FILE,
 )
 
 
@@ -28,54 +32,59 @@ Y_TEST_PAH = os.path.join(MODEL_DIR, Y_TEST_FILE)
 
 
 model = None
-with open(MODEL_PATH, "rb") as f:
-    try:
+try:
+    with open(MODEL_PATH, "rb") as f:
         model = pickle.load(f)
-    except Exception:
-        print("Ocorreu um erro ao carregar o modelo")
+except Exception:
+    print("Ocorreu um erro ao carregar o modelo")
     
 
 X_test = None
-with open(X_TEST_PAH, "rb") as f:
-    try:
+try:
+    with open(X_TEST_PAH, "rb") as f:
         X_test = pickle.load(f)
-    except Exception:
-        print(
-            "Ocorreu um erro ao carregar o dados de teste de X"
-        )
+except Exception:
+    print(
+        "Ocorreu um erro ao carregar o dados de teste de X"
+    )
 
 y_test = None
-with open(Y_TEST_PAH, "rb") as f:
-    try:
+try:
+    with open(Y_TEST_PAH, "rb") as f:
         y_test = pickle.load(f)
-    except Exception:
-        print(
-            "Ocorreu um erro ao carregar o dados de teste de y"
-        )
-
-
-class Features(BaseModel):
-    features: List[List[float]]
+except Exception:
+    print(
+        "Ocorreu um erro ao carregar o dados de teste de y"
+    )
 
 
 @app.get(
     "/predict",
-    summary="Download do modelo treinado"
+    summary="Predição do Modelo com Dados de Testes",
+    description="Realiza a predição do modelo utilizando os dados utilizados no teste do modelo."
 )
-async def get_predict(data: Features):
-    if model:
-        previsao = model.predict(data.features).tolist()
+async def get_predict_test():
+    if model is not None and X_test is not None:
+        previsao = model.predict(X_test).tolist()
         return {"prediction": previsao}
-    return {"erro": "Modelo não pode ser carregado"}
+    return JSONResponse(
+        status_code=500, 
+        content={"msg": "Modelo ou dados de Testes de X não podem ser carregados"}
+    )
 
 
 @app.get(
     "/report",
-    response_class=PlainTextResponse
+    response_class=PlainTextResponse,
+    summary="Relatório de Classificação",
+    description="Retorna o relatório de Classificação com base nos Dados de Testes"
 )
 def report():
-    if model and X_test and y_pred:
+    if model is not None and X_test is not None and y_test is not None:
         y_pred = model.predict(X_test)
         relatorio = classification_report(y_test, y_pred)
         return relatorio
-    return "Modelo ou dados de Testes de X e y não podem ser carregados."
+    return JSONResponse(
+        status_code=500, 
+        content={"msg": "Modelo ou dados de Testes de X e y não podem ser carregados"}
+    )
